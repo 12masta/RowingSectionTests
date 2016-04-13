e5a9ab3b04613125dc7aaa56786bd06160ae1a16
Marcin Stanek <stanek.marcinp@gmail.com>
2016-04-13 23:01:26 +0200

    untracked commit

---
+++ RowingSectionTests/HomePageObjects.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RowingSectionTests
+{
+    class HomePageObjects
+    {
+        public HomePageObjects()
+        {
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "loginLink")]
+        public IWebElement btnToLogin { get; set; }
+
+        public LoginPageObjects ToLoginPage()
+        {
+            btnToLogin.Clicks();
+        
+            return new LoginPageObjects();
+        }
+
+    }
+}
